a3b6ca52271c8e32c2d87624bbdd209884de2e4a
Chris <chevalier@chris-freelance.com>
2025-12-11 10:54:11 +0100

    Add Doctrine entities

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
 use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class PostController extends AbstractController
 {
     #[Route("/post/create", name: "post_create")]
-    public function create(Request $request): Response
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $post = new Post(); // Article vide
         $form = $this->createForm(PostType::class, $post);
@@ -21,8 +22,48 @@ class PostController extends AbstractController
         $form->handleRequest($request); // L'objet 'post' est hydraté
         if ($form->isSubmitted() && $form->isValid()) {
             $post->setPublishedAt(new DateTime());
-            dump($post);
+            $em = $doctrine->getManager();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute("home");
         }
         return $this->render("post/form.html.twig", ["form" => $form->createView()]);
+    }
+
+    #[Route("/post/edit/{id}", name: "post_edit")]
+    public function edit(Request $request, ManagerRegistry $doctrine, Post $post): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request); // L'objet 'post' est réhydraté
+        if ($form->isSubmitted() && $form->isValid()) {
+            /* $post->setPublishedAt(new DateTime()); */
+            $em = $doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute("home");
+        }
+        return $this->render("post/form.html.twig", ["form" => $form->createView()]);
+    }
+
+    #[Route("/", name: "home")]
+    public function readAll(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Post::class);
+        return $this->render("post/home.html.twig", ["posts" => $repository->findAll()]);
+    }
+
+    #[Route("post/read/{id}", name: "read_post")]
+    public function read(Post $post): Response
+    {
+        return $this->render("post/read.html.twig", ["post" => $post]);
+    }
+
+    #[Route("post/delete/{id}", name: "delete_post")]
+    public function delete(ManagerRegistry $doctrine, Post $post): Response
+    {
+        $em = $doctrine->getManager();
+        $em->remove($post);
+        $em->flush();
+        return $this->redirectToRoute("home");
     }
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,10 +3,17 @@
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity]
 class Post
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private int $id;
 
     #[Assert\NotBlank(message: "Le titre de l'article ne doit pas être vide !")]
@@ -16,6 +23,7 @@ class Post
         minMessage: "Le titre de l'article doit faire plus de 10 caractères.",
         maxMessage: "Le titre de l'article doit faire moins de 150 caractères."
     )]
+    #[ORM\Column(unique: true)]
     private string $title;
 
     #[Assert\NotBlank(message: "Le contenu de l'article ne doit pas être vide !")]
@@ -25,9 +33,25 @@ class Post
         minMessage: "Le contenu de l'article doit faire plus de 10 caractères.",
         maxMessage: "Le contenu de l'article doit faire moins de 150 caractères."
     )]
+    #[ORM\Column(type: "text")]
     private string $content;
-    private string $image;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $image = NULL;
+
+    #[ORM\Column]
     private DateTime $publishedAt;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'post', orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): int
     {
@@ -108,6 +132,36 @@ class Post
     public function setPublishedAt(DateTime $publishedAt): self
     {
         $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
 
         return $this;
     }
